4b8e6d9aa17014c6072c6cfbf3f87f9447865f2a
Zuo Yang <zuya@microsoft.com>
2018-08-08 20:13:01 +0800

    update utils

--- DotUtils.cs
+++ DotUtils.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Tree.BST;
 
 namespace Tree.Utils {
-    public static class DotUtils<T> where T : IComparable<T> {
+    public static class BSTUtils<T> where T : IComparable<T> {
 
         public static string generateDotString(BSTTreeNode<T> root) {
             var contents = new List<string>();
@@ -46,5 +46,51 @@ namespace Tree.Utils {
         }
 
         private static int _Count = 0;
-    } 
+    }
+
+    public static class RBUtils<T> where T : IComparable<T> {
+
+        public static string generateDotString(RBTreeNode<T> root, RBTreeNode<T> sentryNode) {
+            var contents = new List<string>();
+            var dictionary = new Dictionary<RBTreeNode<T>, string>();
+            PreOrderTraversal(root, sentryNode, contents, dictionary);
+            foreach (var item in dictionary) {
+                string color = item.Key.Color == RBColor.BLACK ? "blue" : "red";
+                contents.Insert(0, $"node [color={color}];\n{item.Value}[label = \"<f0> |<f1> {item.Key.Value}|<f2> \"];");
+            }
+            var result = $"digraph g {{\nnode [shape = record,height=.1]; {string.Join("\n", contents)} \n}}";
+            return result;
+        }
+
+        private static void PreOrderTraversal(RBTreeNode<T> root, RBTreeNode<T> sentryNode, List<string> contents, Dictionary<RBTreeNode<T>, string> dictionary) {
+            if (root != sentryNode) {
+
+                var rootName = GetTreeNodeName(root, dictionary);
+                if (root.Left != sentryNode) {
+                    var leftName = GetTreeNodeName(root.Left, dictionary);
+                    contents.Add($"\"{rootName}\":f0 -> \"{leftName}\":f1;");
+                }
+                if (root.Right != sentryNode) {
+                    var rightName = GetTreeNodeName(root.Right, dictionary);
+                    contents.Add($"\"{rootName}\":f2 -> \"{rightName}\":f1;");
+                }
+
+                PreOrderTraversal(root.Left, sentryNode, contents, dictionary);
+                PreOrderTraversal(root.Right, sentryNode, contents, dictionary);
+            }
+        }
+
+        private static string GetTreeNodeName(RBTreeNode<T> node, Dictionary<RBTreeNode<T>, string> dictionary) {
+            if (dictionary.ContainsKey(node)) {
+                return dictionary[node];
+            }
+
+            var nodeName = $"node{_Count}";
+            dictionary[node] = nodeName;
+            _Count++;
+            return nodeName;
+        }
+
+        private static int _Count = 0;
+    }
 }
