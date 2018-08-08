51600067e91cf94e06659b283d71422f0d17ece8
Zuo Yang <zuya@microsoft.com>
2018-08-08 16:00:45 +0800

    add some implementation

--- RedBlackTree.cs
+++ RedBlackTree.cs
@@ -30,23 +30,67 @@ namespace Tree.BST {
         }
 
         public RBTreeNode<T> Search(T key) {
-            throw new NotImplementedException();
+            var current = _Root;
+            while (current != null && current.Value.CompareTo(key) != 0) {
+                if (current.Value.CompareTo(key) > 0) {
+                    current = current.Left;
+                } else {
+                    current = current.Right;
+                }
+            }
+            return current;
         }
 
         public RBTreeNode<T> Maximum() {
-            throw new NotImplementedException();
+            if (_Root == null) {
+                throw new ArgumentNullException();
+            }
+
+            return _Maximum(_Root);
         }
 
         public RBTreeNode<T> Minimum() {
-            throw new NotImplementedException();
+            if (_Root == null) {
+                throw new ArgumentNullException();
+            }
+
+            return _Minimum(_Root);
         }
 
         public RBTreeNode<T> Successor(RBTreeNode<T> node) {
-            throw new NotImplementedException();
+            if (node == null) {
+                throw new ArgumentNullException();
+            }
+
+            if (node.Right != null) {
+                return _Maximum(node.Right);
+            }
+
+            var current = node;
+            var parent = current.Parent;
+            while (parent != _SentryNode && parent.Right == current) {
+                current = parent;
+                parent = parent.Parent;
+            }
+            return parent;
         }
 
         public RBTreeNode<T> Predecessor(RBTreeNode<T> node) {
-            throw new NotImplementedException();
+            if (node == null) {
+                throw new ArgumentNullException();
+            }
+
+            if (node.Left != null) {
+                return _Maximum(node.Left);
+            }
+
+            var current = node;
+            var parent = node.Parent;
+            while (parent != _SentryNode && parent.Left == current) {
+                current = parent;
+                parent = parent.Parent;
+            }
+            return parent;
         }
 
         private RBTreeNode<T> Left_Rotate(RBTreeNode<T> node) {
@@ -70,18 +114,34 @@ namespace Tree.BST {
         }
 
         private RBTreeNode<T> _Root;
-        private RBTreeNode<T> _SentryNode;
+        private RBTreeNode<T> _SentryNode = new RBTreeNode<T>(RBColor.BLACK);
+
+        private RBTreeNode<T> _Maximum(RBTreeNode<T> node) {
+            var current = node;
+            while (current.Right != _SentryNode) {
+                current = current.Right;
+            }
+            return current;
+        }
+
+        private RBTreeNode<T> _Minimum(RBTreeNode<T> node) {
+            var current = node;
+            while (current.Left != _SentryNode) {
+                current = current.Left;
+            }
+            return current;
+        }
     }
 
     public class RBTreeNode<T> where T : IComparable<T> {
-        public RBTreeNode(T t) => (Value, Color) = (t, RBColor.BLACK);
-        public RBTreeNode(T t, RBColor color) => (Value, Color) = (t, color);
+        public RBTreeNode(T t) => (Value, Color) = (t, RBColor.RED);
+        public RBTreeNode(RBColor color) => (Color) = (color);
 
         public T Value { get; set; }
         public RBColor Color { get; set; }
-        public BSTTreeNode<T> Left { get; set; }
-        public BSTTreeNode<T> Right { get; set; }
-        public BSTTreeNode<T> Parent { get; set; }
+        public RBTreeNode<T> Left { get; set; }
+        public RBTreeNode<T> Right { get; set; }
+        public RBTreeNode<T> Parent { get; set; }
     }
 
     public enum RBColor {
